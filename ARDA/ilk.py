9cf3de52a0691e821f97ccdeb2c36a803bb6078c
Merciful423 <ardagore423@gmail.com>
2020-11-21 16:59:43 +0300

    arda

--- ARDA/ilk.py
+++ ARDA/ilk.py
@@ -1,17 +1,30 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow,QPushButton
+from PyQt5 import uic
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi(r"EDIZ\ilk.ui",self)  # graphical user interface
         self.initUI()
-
+    
     def initUI(self):
-        self.setGeometry(300,300,300,220)
-        self.setWindowTitle("İlk Pencere")
+        self.tbl.setRowCount(10)
+        self.tbl.setColumnCount(2)
+        self.btGiris.clicked.connect(self.girisYap)
+        self.btIptal.clicked.connect(self.temizle)
         self.show()
 
-if __name__== "__main__":
+    def girisYap(self):
+        pass
+
+
+    def temizle(self):
+        self.txtUserName.setText("")
+        self.txtSifre.setText("")
+        self.close()
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex=App()
-    sys.exit(app.exec())
+    ex = App()
+    sys.exit(app.exec_())
