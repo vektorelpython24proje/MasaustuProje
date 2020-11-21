1238abe444acac918f29777a0e42dc0a0a71405e
vektorelpython24 <73065625+vektorelpython24@users.noreply.github.com>
2020-11-21 16:15:57 +0300

    21112020

--- EDIZ/ilkUI_py.py
+++ EDIZ/ilkUI_py.py
@@ -1,21 +1,20 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow,QPushButton
-from PyQt5 import uic
+from ilkUI import Ui_MainWindow
 
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi(r"EDIZ\ilk.ui",self)  # graphical user interface
+        self.ui = Ui_MainWindow()
         self.initUI()
     
     def initUI(self):
-
-        self.btIptal.clicked.connect(self.tiklandi)
+        self.ui.setupUi(self)
+        self.ui.btIptal.clicked.connect(self.tiklandi)
         self.show()
 
     def tiklandi(self):
-        self.txt
-
+        self.ui.txtUserName.setText("Merhaba")
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = App()
