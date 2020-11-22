434ea0fb3d8810603884f30138f5261435240fae
vektorelpython24 <73065625+vektorelpython24@users.noreply.github.com>
2020-11-22 15:59:11 +0300

    22112020

--- EDIZ/Kamera.py
+++ EDIZ/Kamera.py
@@ -9,17 +9,55 @@ import cv2
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.timer = QTimer()
         uic.loadUi(r"EDIZ\Kamera.ui",self)  # graphical user interface
         self.initUI()
 
     def initUI(self):
+
+        self.btAc.clicked.connect(self.KameraAc)
+        self.btKapat.setEnabled(False)
+        self.btKapat.clicked.connect(self.KameraKapat)
         self.show()
 
     def KameraAc(self):
-        pass
+        if not self.timer.isActive():
+            self.cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+            self.timer.start(3)
+            self.btAc.setEnabled(False)
+            self.btKapat.setEnabled(True)
+            self.Goster()
+        else:
+            self.cam.release()
+            self.timer.stop()
+
+        
+
+    def Goster(self):
+        while True:
+            ret,frame = self.cam.read()
+            buyukFaktor = 0.6
+            frame = cv2.resize(frame,None,fx=buyukFaktor,fy=buyukFaktor,interpolation=cv2.INTER_AREA)
+            height,width,channel = frame.shape  # (640,480,3)
+            step = channel*width # (3*480)
+            #-----------------
+            qImg = QImage(frame.data,width,height,step,QImage.Format_BGR888)  # farklı kanallardan aldığı veriyi
+            # bir resim olarak ekrana yansıtmak üzere QImage kullanıldı
+            self.lblCam.setPixmap(QPixmap.fromImage(qImg)) # Alınan resmi label içerisinde gösterebilmek için
+            # QPixmap kullanıldı
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+        self.cam.release()
+        self.timer.stop()    
 
     def KameraKapat(self):
-        pass
+        try:
+            self.cam.release()
+        except:
+            pass
+        self.timer.stop()
+        self.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
