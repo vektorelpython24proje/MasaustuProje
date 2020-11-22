a15331a4caa2a60ef384b60d33435ac28bb998e1
Merciful423 <ardagore423@gmail.com>
2020-11-22 14:54:36 +0300

    arda

--- ARDA/ilk.py
+++ ARDA/ilk.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow,QPushButton
-from PyQt5 import uic
+from PyQt5 import uic,QtWidgets
 
 class App(QMainWindow):
     def __init__(self):
@@ -9,15 +9,25 @@ class App(QMainWindow):
         self.initUI()
     
     def initUI(self):
-        self.tbl.setRowCount(10)
+        self.tbl.setRowCount(1)
         self.tbl.setColumnCount(2)
         self.btGiris.clicked.connect(self.girisYap)
         self.btIptal.clicked.connect(self.temizle)
+        self.cmb.currentTextChanged.connect(self.sonuc)
         self.show()
 
     def girisYap(self):
-        pass
+        numRows = self.tbl.rowCount()
+        kullanici = QtWidgets.QTableWidgetItem(self.txtUserName.text())
+        sifre = QtWidgets.QTableWidgetItem(self.txtSifre.text())
+        self.tbl.setItem(numRows-1,0,kullanici)
+        self.tbl.setItem(numRows-1,1,sifre)
+        self.tbl.setRowCount(numRows+1)
+        self.cmb.addItem(self.txtUserName.text())
 
+    def sonuc(self):
+        sonuc = self.cmb.currentText()
+        self.lblSonuc.setText(sonuc)
 
     def temizle(self):
         self.txtUserName.setText("")
