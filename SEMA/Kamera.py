453b09c8df3dddb8bc0d630a80013aca9783408b
SemaBulut <sema.bulut.tedu@gmail.com>
2020-11-22 16:07:00 +0300

    221120202

--- SEMA/Kamera.py
+++ SEMA/Kamera.py
@@ -9,17 +9,50 @@ import cv2
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.timer = QTimer()
         uic.loadUi(r"SEMA\Kamera.ui",self)
         self.initUI()
 
     def initUI(self):
+        self.btAc.clicked.connect(self.KameraAc)
+        self.btKapat.setEnabled(False)
+        self.btKapat.clicked.connect(self.KameraKapat)
         self.show()
 
     def KameraAc(self):
-        pass
+        if not self.timer.isActive():
+            self.cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+            self.timer.start(3)
+            self.btAc.setEnabled(False)
+            self.btKapat.setEnabled(True)
+            self.Goster()
+        else:
+            self.cam.release()
+            self.timer.stop()
+    
+    def Goster(self):
+        while True:
+            ret,frame = self.cam.read()
+            buyukFaktor = 0.4
+            frame = cv2.resize(frame,None,fx=buyukFaktor,fy=buyukFactor,interpolation=cv2.INTER_AREA)
+            height,width,channel = frame.shape
+            step = channel*width
+
+            qImg = QImage(frame.data,width,height,step,QImage.Format_BGR888)
+            self.lblCam.setPixmap(QPixmap.fromImage(qImg))
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        
+        self.cam.release()
+        self.timer.stop()
+
+
 
     def KameraKapat(self):
-        pass
+        try:
+            self.cam.release()
+        except:
+            pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
